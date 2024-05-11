33bdd8f2901c52f996618b1c44070ab757b04cb9
Tmehnaz <tanjinamehanz@gmail.com>
2024-05-11 17:02:29 +0600

    session_id added with runtime error

--- data_collection.js
+++ data_collection.js
@@ -1,10 +1,38 @@
 const express = require('express');
 const db = require('./dbConnection');
+const { nanoid } = require('nanoid');
 const router = express.Router();
+
+//Homepage GET request
+
+router.get('/sessionid', (req,res)=>{
+    try{
+        req.session_id = nanoid(8);
+        if(!session_id){
+            return res.status(404).send({
+                success:false,
+                message:"Session ID was not found"  
+            })
+        }
+        res.status(201).send({
+            success:true,
+            message:"Session_id has been created",
+            session_id
+        })
+    }catch(err){
+        console.log("Couldn't genrate session id");
+        res.status(500).send({
+            success:false,
+            message:"Error in generating session_id",
+            err
+        })
+    }
+}) 
 
 //Homepage data colelction
 router.post('/collectdata',async (req,res)=>{
     try{
+        
      const {session_id,age_group,ethnicity,custom_ethnicity,gender,custom_gender,country,education,marital_status,
         employment, income,household_size,home_ownership,bestfriend,
         authority_figure,environmental_choice,medication_ack,vision_ack} = req.body;
